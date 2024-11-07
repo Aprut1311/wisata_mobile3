97fb03b7e0395b55b76f9b6b6d29577a4b0a60b4
Aprut1311 <daffahermansyah14@gmail.com>
2024-11-07 09:51:09 +0700

    membuat landing page

---
+++ lib/model/landing_model.dart
@@ -0,0 +1,20 @@
+class LandingModel {
+  String image, name;
+
+  LandingModel({required this.image, required this.name});
+}
+
+List<LandingModel> landingpage = [
+  LandingModel(
+      image:
+          'https://i.redd.it/gofamdij6r661.png',
+      name: 'Jelajahi Indonesia Bersama Kami'),
+  LandingModel(
+      image:
+          'https://th.bing.com/th/id/OIP.zqFBDPk2H56bjhJmX1CRegHaMI?w=900&h=1474&rs=1&pid=ImgDetMain',
+      name: 'Keindahan Alam Indonesia'),
+  LandingModel(
+      image:
+          'https://th.bing.com/th/id/OIP.xjPMH20oytTZMErsZw37fgHaI4?rs=1&pid=ImgDetMain',
+      name: 'Kesejukan Angin Pegunungan'),
+];
